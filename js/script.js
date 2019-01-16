73b8cdafe24acaa8e90664ed3d098a227744c1ab
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-01-16 19:33:55 +0100

    install flickility hash

--- js/script.js
+++ js/script.js
@@ -8,9 +8,39 @@ var flkty = new Flickity(elem, {
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity('.main-carousel', {
+    pageDots: false,
     // options
 });
 
 
 
-pageDots: false
+function listener( /* parameters */ ) {
+    console.log('eventName happened');
+}
+// bind event listener
+flkty.on('eventName', listener);
+// unbind event listener
+flkty.off('eventName', listener);
+// bind event listener to trigger once. note ONCE not ONE or ON
+flkty.once('eventName', function () {
+    console.log('eventName happened just once');
+});
+
+
+var flkty = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+
+
+var flkty = new Flickity('.main-carousel');
+
+var firstsButton = document.querySelector('.button--first');
+firstsButton.addEventListener('click', function () {
+    flkty.reset();
+});
