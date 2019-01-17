65d6ecf51a15c7c1f007ab392d2be1fa84aefe04
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-01-17 16:17:36 +0100

    install flickity --save-dev

--- js/script.js
+++ js/script.js
@@ -1,46 +1,14 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity(elem, {
-    // options
-    cellAlign: 'left',
-    contain: true
-});
-
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity('.main-carousel', {
-    pageDots: false,
-    // options
+var flkty = new Flickity('.main-carousel', {});
+// restart function
+var restart = document.querySelector('.restart');
+restart.addEventListener("click", function (event) {
+    flkty.select(0);
 });
-
-
-
-function listener( /* parameters */ ) {
-    console.log('eventName happened');
-}
-// bind event listener
-flkty.on('eventName', listener);
-// unbind event listener
-flkty.off('eventName', listener);
-// bind event listener to trigger once. note ONCE not ONE or ON
-flkty.once('eventName', function () {
-    console.log('eventName happened just once');
-});
-
-
-var flkty = new Flickity('.main-carousel');
-
+// progress bar
 var progressBar = document.querySelector('.progress-bar')
-
 flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
-});
-
-
-
-var flkty = new Flickity('.main-carousel');
-
-var firstsButton = document.querySelector('.button--first');
-firstsButton.addEventListener('click', function () {
-    flkty.reset();
 });
