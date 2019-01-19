13930641ae4624258799c9e8ccbc7bec31ff25e0
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-01-19 17:04:05 +0100

    Etap1 zad.10.3 nie zrobione

--- js/script.js
+++ js/script.js
@@ -12,36 +12,24 @@ mainElement.innerHTML = carouselElements;
 
 // Dots and hash
 var mainElement = document.querySelector('.carousel');
-
 var flkty = new Flickity(mainElement, {
-    // options
     cellAlign: 'left',
-    //disable dots
-    pageDots: false,
-    // Enable hash behavior
-    hash: true,
+    pageDots: false, //usuwa kropki pod karuzelą - DZIAŁA
+    hash: true //włącza hash (odnosniki do linkow) - NIE DZIAŁA
 });
 
 // progress bar scroll
 var progressBar = document.querySelector('.progress-bar');
-flkty.on('scroll', function (progress) {
+flkty.on('scroll', function (progress) { //przesuwanie slajdów
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
 
-// Use button to restart
-var buttonGroup = document.querySelector('.button-group');
-var buttons = buttonGroup.querySelectorAll('.restart');
-buttons = fizzyUIUtils.makeArray(buttons);
-buttonGroup.addEventListener('click', function (event) {
-    // filter for button clicks
-    if (!matchesSelector(event.target, '.restart')) {
-        return;
-    }
-    var index = buttons.indexOf(event.target);
-    flkty.select(index);
+// BUTTON RESETUJACY I POWROT DO PIERWSZEGO SLAJDU
+var restart = document.querySelector(".restart");
+restart.addEventListener("click", function () {
+    flkty.select(0);
 });
-
 
 //MAPA GOOGLE
 window.initMap = function () {
@@ -49,27 +37,26 @@ window.initMap = function () {
     var photoLocation = slideData[0].coords; // pobiera współrzędne miejsc z kodu html. DZIAŁA
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-        center: photoLocation //DZIAŁA
+        center: photoLocation, //DZIAŁA
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
     //DODAWANIE MARKERóW W PĘTLI
-    var markerMap = [];
-    for (var i = 0; i < slideData.length; i++) {
+    var markerMap = []; //tablica wszystkich pięciu markerów
+    for (var i = 0; i < slideData.length; i++) { //petla po calym kodzie z html
         markerMap.push(new google.maps.Marker({
-            position: slideData[i].coords,
-            map: map
+            position: slideData[i].coords, //dodanie po kolei kazdej lokalizacji
+            map: map, //na mapie
         })); //DZIAŁA (5 markerów na stronie)
+        // NIE DZIALA
+        markerMap[i].addListener("click", function () {
+            flkty.select(i); //tylko to działa z odwolaniem do 0- tresc zadania : Wewnątrz tej akcji ma wykonywać się bardzo podobny kod do tego, który wykonuje się w reakcji na kliknięcie guzika "Reset". Jedyna różnica, to że zamiast do slajdu o indeksie 0, karuzela ma się przewijać do slajdu o indeksie i (zakładając, że Twoja pętla używa zmiennej i jako iteratora).
+        })
     }
+    //Kada zmiana slajdu zmienia pozycje markera na mapie DZIALA 
+    flkty.on("change", function (index) {
+        map.panTo(slideData[index].coords);
+        map.setZoom(5);
+    });
 
-
-    // Kliknięcie na marker - przeniesienie do zdjęcia ze slajdu
-    //1. Pętla przez wszystkie markery, 
-    //2. Event - przycisniecie markera [i] przenosi do slide.data[i].image
-    // 3 Przesuwanie slajdu, zmiana pozycji na mapie
-    //  for (var i = 0; i < markerMap.length; i++) {
-    //      marker[i].addListener('click', function () {
-    //       map.panTo(slideData[i].image);
-    // map.setZoom(5);
-    //      });
-    ////   }
 }
